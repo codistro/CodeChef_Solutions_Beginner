5ff681e6ce2c0b151a82b21015c39a6846c6658c
rajan <rajan191455@gmail.com>
2016-11-16 23:35:51 +0530

    added 3 solutions 16/11/16

---
+++ FLOW008.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        if(a<10)
+            cout<<"What an obedient servant you are!"<<endl;
+        else
+            cout<<-1<<endl;
+    }
+    return 0;
+}
